1c89623e2a8f8e2059b2633c5574505a985d7f21
Andre Sandoval <43323177+andremac15@users.noreply.github.com>
2019-09-09 16:48:34 -0500

    Add files via upload

---
+++ StudentRegistrationDemo3/StudentRegistrationDemo3/Controllers/StudentDeleteController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+using StudentRegistrationDemo3.Models;
+
+namespace StudentRegistrationDemo3.Controllers
+{
+    [Route("api/[controller]")]
+    public class StudentDeleteController : Controller
+    {
+        [Route("student/remove/{regdNum}")]
+        // DELETE: api/<controller>
+        [HttpDelete]
+        public IActionResult DeleteStudentRecord(String regdNum)
+        {
+            Console.WriteLine("In deleteStudentRecord");
+            return Ok(StudentRegistration.getInstance().Remove(regdNum));
+        }
+    }
+}
